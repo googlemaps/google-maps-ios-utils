ddeff2ded0157cdb4f615f0c114661c7d10673ab
Colin Edwards <colin@recursivepenguin.com>
2014-02-12 00:06:13 -0600

    Render clusters as circles with a number

--- Clustering/View/GDefaultClusterRenderer.m
+++ Clustering/View/GDefaultClusterRenderer.m
@@ -1,3 +1,4 @@
+#import <CoreText/CoreText.h>
 #import "GDefaultClusterRenderer.h"
 #import "GQuadItem.h"
 #import "GCluster.h"
@@ -25,7 +26,7 @@
         [markerCache addObject:marker];
         
         if ([cluster count] > 1) {
-            marker.icon = [GMSMarker markerImageWithColor:[UIColor blueColor]];
+            marker.icon = [self generateClusterIconWithCount:[cluster count]];
         }
         else {
             marker.icon = [GMSMarker markerImageWithColor:[UIColor greenColor]];
@@ -33,6 +34,75 @@
         marker.position = cluster.position;
         marker.map = map;
     }
+}
+
+- (UIImage*) generateClusterIconWithCount:(int)count {
+    
+    int diameter = 40;
+    float inset = 3;
+    
+    CGRect rect = CGRectMake(0, 0, diameter, diameter);
+    UIGraphicsBeginImageContextWithOptions(rect.size, NO, 0);
+
+    CGContextRef ctx = UIGraphicsGetCurrentContext();
+
+    // set stroking color and draw circle
+    [[UIColor colorWithRed:1 green:1 blue:1 alpha:0.8] setStroke];
+    [[UIColor blueColor] setFill];
+
+    CGContextSetLineWidth(ctx, inset);
+
+    // make circle rect 5 px from border
+    CGRect circleRect = CGRectMake(0, 0, diameter, diameter);
+    circleRect = CGRectInset(circleRect, inset, inset);
+
+    // draw circle
+    CGContextFillEllipseInRect(ctx, circleRect);
+    CGContextStrokeEllipseInRect(ctx, circleRect);
+
+    CTFontRef myFont = CTFontCreateWithName( (CFStringRef)@"Helvetica-Bold", 18.0f, NULL);
+    
+    NSDictionary *attributesDict = [NSDictionary dictionaryWithObjectsAndKeys:
+            (__bridge id)myFont, (id)kCTFontAttributeName,
+                    [UIColor whiteColor], (id)kCTForegroundColorAttributeName, nil];
+
+    // create a naked string
+    NSString *string = [[NSString alloc] initWithFormat:@"%d", count];
+
+    NSAttributedString *stringToDraw = [[NSAttributedString alloc] initWithString:string
+                                                                       attributes:attributesDict];
+
+    // flip the coordinate system
+    CGContextSetTextMatrix(ctx, CGAffineTransformIdentity);
+    CGContextTranslateCTM(ctx, 0, diameter);
+    CGContextScaleCTM(ctx, 1.0, -1.0);
+
+    CTFramesetterRef frameSetter = CTFramesetterCreateWithAttributedString((__bridge CFAttributedStringRef)(stringToDraw));
+    CGSize suggestedSize = CTFramesetterSuggestFrameSizeWithConstraints(
+                                                                        frameSetter, /* Framesetter */
+                                                                        CFRangeMake(0, stringToDraw.length), /* String range (entire string) */
+                                                                        NULL, /* Frame attributes */
+                                                                        CGSizeMake(diameter, diameter), /* Constraints (CGFLOAT_MAX indicates unconstrained) */
+                                                                        NULL /* Gives the range of string that fits into the constraints, doesn't matter in your situation */
+                                                                        );
+    CFRelease(frameSetter);
+    
+    //Get the position on the y axis
+    float midHeight = diameter;
+    midHeight -= suggestedSize.height;
+    
+    float midWidth = diameter / 2;
+    midWidth -= suggestedSize.width / 2;
+
+    CTLineRef line = CTLineCreateWithAttributedString(
+            (__bridge CFAttributedStringRef)stringToDraw);
+    CGContextSetTextPosition(ctx, midWidth, 12);
+    CTLineDraw(line, ctx);
+
+    UIImage *image = UIGraphicsGetImageFromCurrentImageContext();
+    UIGraphicsEndImageContext();
+
+    return image;
 }
 
 @end
