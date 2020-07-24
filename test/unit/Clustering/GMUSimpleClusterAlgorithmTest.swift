340c65c2a56d5921442896aed5e3d7a74440642b
Subhodip Banerjee <differentialthinking@gmail.com>
2020-07-25 00:13:39 +0530

    test: Coverage increased to 75.1%

---
+++ test/unit/Clustering/GMUSimpleClusterAlgorithmTest.swift
@@ -0,0 +1,54 @@
+/* Copyright (c) 2020 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import XCTest
+@testable import GoogleMapsUtils
+
+class GMUSimpleClusterAlgorithmTest: GMUClusterAlgorithmTest {
+  
+  private var clustersCount: Int!
+  
+  override func setUp() {
+    clustersCount = 10
+    super.setUp()
+  }
+  
+  override func tearDown() {
+    clustersCount = nil
+    super.tearDown()
+  }
+  
+  func testClustersAtZoomWithTenClusterCount() {
+    let simpleClusterAlgorithm = GMUSimpleClusterAlgorithm()
+    simpleClusterAlgorithm.add(self.simpleClusterItems())
+    simpleClusterAlgorithm.add(self.simpleClusterItems())
+    simpleClusterAlgorithm.add(self.simpleClusterItems())
+    let clusterItems = simpleClusterAlgorithm.clusters(atZoom: 3)
+    XCTAssertEqual(clustersCount, clusterItems.count)
+  }
+  
+  func testClustersAtZoomWithRemovingAndClearingClusterItems() {
+    let simpleClusterAlgorithm = GMUSimpleClusterAlgorithm()
+    simpleClusterAlgorithm.add(self.simpleClusterItems())
+    simpleClusterAlgorithm.add(self.simpleClusterItems())
+    simpleClusterAlgorithm.add(self.simpleClusterItems())
+    simpleClusterAlgorithm.remove(self.simpleClusterItems()[0])
+    simpleClusterAlgorithm.clearItems()
+    let clusterItems = simpleClusterAlgorithm.clusters(atZoom: 3)
+    XCTAssertEqual(0, clusterItems.count)
+    XCTAssertNotEqual(clustersCount, clusterItems.count)
+  }
+  
+}
