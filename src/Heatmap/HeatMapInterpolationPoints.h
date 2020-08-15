3513bd404a8f6eae17ecee7c51c70cb8ba1a1c5c
Haiming Xu <haimingericxu@gmail.com>
2020-08-15 11:24:20 -0400

    added h file

---
+++ src/Heatmap/HeatMapInterpolationPoints.h
@@ -0,0 +1,9 @@
+//
+//  HeatMapInterpolationPoints.h
+//  GoogleMapsUtils
+//
+//  Created by Haiming Xu on 8/15/20.
+//  Copyright © 2020 Google. All rights reserved.
+//
+
+@class HeatMapInterpolationPoints;
