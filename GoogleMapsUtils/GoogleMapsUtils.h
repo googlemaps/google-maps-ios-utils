4451c5c89c9aa92534fc4c43b6eb2ed33f425a27
Haiming Xu <haimingericxu@gmail.com>
2020-08-15 10:58:29 -0400

    added @class

--- GoogleMapsUtils/GoogleMapsUtils.h
+++ GoogleMapsUtils/GoogleMapsUtils.h
@@ -27,7 +27,7 @@ FOUNDATION_EXPORT const unsigned char GoogleMapsUtilsVersionString[];
 #import <GoogleMapsUtils/GMUGradient.h>
 #import <GoogleMapsUtils/GMUHeatmapTileLayer.h>
 #import <GoogleMapsUtils/GMUWeightedLatLng.h>
-#import <GoogleMapsUtils/GoogleMapsUtils-Swift.h>
+@class HeatMapInterpolationPoints;
 
 // Clustering
 #import <GoogleMapsUtils/GMUClusterAlgorithm.h>
